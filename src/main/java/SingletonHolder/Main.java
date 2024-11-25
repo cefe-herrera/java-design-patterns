ba1fcf0924da5f072bc31c0fbe8871555f574c7b
Armando Herrera <armandoher01@gmail.com>
2024-11-25 20:54:26 -0300

    add holder singleton

---
+++ src/main/java/SingletonHolder/Main.java
@@ -0,0 +1,15 @@
+package SingletonHolder;
+
+public class Main {
+
+    public static void main(String[] args){
+        Singleton singleton1 = Singleton.getInstance();
+
+        Singleton singleton2 = Singleton.getInstance();
+
+        if(singleton1 == singleton2){
+            System.out.println("Instances are equals");
+        }
+    }
+
+}
