05f733c4986bf8c433fbe06685bf9f9d5b31b6be
Armando Herrera <armandoher01@gmail.com>
2024-11-26 21:31:21 -0300

    add prototype pattern

---
+++ src/main/java/prototype/Main.java
@@ -0,0 +1,18 @@
+package prototype;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car originalCar = new Car("Tesla", "Model S", 2023, "Red");
+        System.out.println("Original Car: " + originalCar);
+
+        Car clonedCar = originalCar.clone();
+        System.out.println("Cloned Car: " + clonedCar);
+
+
+        // Modify copy not original
+        clonedCar.setColor("Blue");
+        System.out.println("Modified Cloned Car: " + clonedCar);
+        System.out.println("Original Car remains unchanged: " + originalCar);
+    }
+}
